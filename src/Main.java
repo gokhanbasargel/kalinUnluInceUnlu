669d9c4a0483bc08143a3d7b4a15c00f33579367
gokhanBasargel <gokhan.basergel@gmail.com>
2022-09-14 18:14:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+       char harf = 'O';
+
+       switch (harf){
+           case 'A':
+           case 'I':
+           case 'O':
+           case 'U':
+
+               System.out.println("Kalın Ünlüdür");
+            break;
+        }
+        switch (harf){
+            case 'E':
+            case 'İ':
+            case 'Ü':
+            case 'Ö':
+                System.out.println("İnce Ünlüdür.");
+            break;
+            
+        }
+    }
+}
